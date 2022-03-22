4a7c50f432b562c3a9d6b423de4918ebcc54a447
rcn4301 <97567547+rcn4301@users.noreply.github.com>
2022-03-22 13:13:48 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome Back, Buccos!</h1>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
